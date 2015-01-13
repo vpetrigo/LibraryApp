9d3af75a89f122c5f883c699b8b63422b3b93dee
Vladimir Petrigo <taenaru@gmail.com>
2015-01-13 17:49:42 +0300

    Change isbn_check() function. Now it uses splitting for check how many parts we pass to our constructor. Then it checks whether it conform of Num-Num-Num-(Num | Alph) pattern or not. Now it is able to handle different weird combination of ISBN like "--456--", "-1-1-1-1" and so on.

--- Books.cpp
+++ Books.cpp
@@ -1,26 +1,35 @@
 #include "Books.h"
 
-bool Book::isbn_check(const string& isbn) {
-  int hyphen = 0;
+bool Book::isbn_check() {
   int pos = 0;
+  int (*analyse_fun)(int); // pointer to char analyser function
 
-  for (int i = 0; i < isbn.size() && hyphen != 3; ++i) {
-    if (isdigit(isbn[i]))
-      continue;
-    else if (isbn[i] == '-') {
-      ++hyphen;
-      pos = i;
-    }
-    else
-      return false;
+  vector<string> isbn_parts;
+
+  while (pos < isbn.size()) { // Here we split ISBN number we have by hyphen
+    string part;
+    for ( ; pos < isbn.size() && isbn[pos] != '-'; ++pos)
+      part += isbn[pos];
+
+    if (isbn[pos] == '-')
+      ++pos;
+
+    isbn_parts.push_back(part);
   }
 
-  if (hyphen < 3)
+  if (isbn_parts.size() != Book::isbn_len)
     return false;
 
-  for (int i = pos + 1; i < isbn.size(); ++i)
-    if (!isalnum(isbn[i]))
+  for (int i = 0; i < Book::isbn_len; ++i) { // check first three parts of ISBN number which has to be numbers
+    if (isbn_parts[i].size() == 0)
       return false;
+
+    analyse_fun = (i != Book::isbn_len - 1) ? (int (*)(int)) isdigit : (int (*)(int)) isalnum;
+
+    for (int j = 0; j < isbn_parts[i].size(); ++j)
+      if (!analyse_fun(isbn_parts[i][j])) // Check if the first three parts of ISBN
+        return false;                     // is numbers and the last one might be
+  }                                       // either number or alphabet characters
 
   return true;
 }
@@ -64,8 +73,8 @@ ostream& operator<<(ostream& os, const Book& b) {
           << "Genre: " << b.genre_str() << '\n';
 }
 
-string Book::genre_str() const {
+const string& Book::genre_str() const {
   static const vector<string> gen_str = {"fiction", "nonfiction", "periodical", "biography", "children"};
 
-  return gen_str[int(genre) - 1];
+  return gen_str[static_cast<int>(genre) - 1];
 }
--- Books.h
+++ Books.h
@@ -9,21 +9,22 @@ enum class Genre {
 class Book {
 public:
     class Invalid {};
+    const int isbn_len = 4; // ISBN pattern is Num-Num-Num-(Num | Alph); 4 parts at all
 
     Book(const string& author, const string& title, const string& isbn, const Chrono::Date& cr_date, bool ch_out, const Genre& gen) :
        author{author}, title{title}, isbn{isbn}, cr_date{cr_date}, check_out{ch_out}, genre{gen} {
-         if (!isbn_check(isbn))
+         if (!isbn_check())
           throw Invalid{};
       }
 
-    bool isbn_check(const string&);
+    bool isbn_check();
     bool have_book();
     const string& isbn_get() const;
     const string& author_get() const;
     const string& title_get() const;
     const Chrono::Date& cdate_get() const;
     const Genre& genre_get() const;
-    string genre_str() const;
+    const string& genre_str() const;
 private:
     bool check_out;
     string isbn;
