f72af02a1e169cef27b6ce8c5c62696a4c3d83f2
Vladimir Petrigo <taenaru@gmail.com>
2015-01-14 16:24:27 +0300

    Add several tests   - Test for checking correctness of reading books   - Test for checking correctness of reading patrons of library   - Output all books and patrons lists   - Transaction functionality test

--- Test.cpp
+++ Test.cpp
@@ -1,37 +1,31 @@
-#include "Books.h"
-#include "Patron.h"
-
-string read_str(); // Used for reading an author name and a title in right format
-Genre read_genre(); // Asked user for genre of the current book
-void read_books(vector<Book>& lib); // Prompt process for obtaining data about a book
+#include "Library.h"
 
 int main() {
     try {
-        vector<Book> library;   // Vector of whole books which library have
-
+        Library my_lib;
         cout << "Welcome to test library program\n"
                 << "You will be prompted about books you have in your library\n"
                 << "Please, answer the questions and have fun.\n";
 
-        //read_books(library);
+        my_lib.read_patrons();
+        my_lib.read_books();
+        Patron v {"Volodja Petrigo", "123-456-RU", 0};
+        Book b {"Alexey Tolstoy", "Upir", "45-987-1-RU", Chrono::Date{2001, Chrono::Month::Jan, 1}, 1, Genre(1)};
+        my_lib.book_chout(v, b);
+        my_lib.book_chout(v, b);
 
-        //for (size_t i = 0; i < library.size(); ++i)
-        //    cout << library[i] << '\n';
+        vector<Patron> clients = my_lib.get_Patrons();
+        vector<Book> books = my_lib.get_Books();
+        vector<Transaction> tr = my_lib.get_Trans();
 
-        /* Patron test {"Ivan Ivanov", "7-1-7987qwe", 100};
+        for (int i = 0; i < clients.size(); ++i)
+            cout << clients[i].getP_name() << '\n';
 
-        cout << test.getP_name() << '\n';
-        cout << test.getLib_cardnum() << '\n';
-        cout << test.getLib_fees() << '\n';
-        cout << test.have_fee() << '\n';
+        for (int i = 0; i < books.size(); ++i)
+            cout << books[i] << '\n';
 
-        test.setLib_cardnum("7-7-7");
-        test.setLib_fees(0);
-
-        cout << test.getP_name() << '\n';
-        cout << test.getLib_cardnum() << '\n';
-        cout << test.getLib_fees() << '\n';
-        cout << test.have_fee() << '\n'; */
+        for (int i = 0; i < tr.size(); ++i)
+            cout << tr[i] << '\n';
 
         return 0;
     }
@@ -71,78 +65,5 @@ int main() {
         cerr << "Something wrong happend\n";
 
         return 4;
-    }
-}
-
-string read_str() { // function for reading author and title
-    string inp;
-    char c = '\0';
-
-    while (cin.get() == '\n') // After each iteration of input the very last symbol in stream will be '\n'
-        ;       // Here we check it and if so skip it
-
-    cin.unget();
-    while (cin.get(c) && c != '\n')
-        inp.push_back(c);
-
-    return inp;
-}
-
-Genre read_genre() {
-    cout << "Please enter the number of genre which mathes this book:\n"
-            << "1 - fiction\n2 - nonfiction\n3 - periodical\n4 - biography\n5 - for children\n";
-
-    int genre = 0;
-
-    cin >> genre;
-
-    return Genre(genre);
-}
-
-void read_books(vector<Book>& lib) {
-    while (cin) {
-        cout << "\nEnter the author: ";
-        string author;
-
-        author = read_str();
-
-        cout << "\nEnter the title: ";
-        string title;
-
-        title = read_str();
-
-        string isbn;
-
-        cout << "\nEnter the ISBN: ";
-
-        cin >> isbn;
-
-        cout << "\nEnter the genre of this book.\n";
-
-        Genre gen = read_genre();
-
-        int year = 0, mon = 0, day = 0;
-
-        cout << "\nEnter the date of release (format: YY MM DD): ";
-        cin >> year >> mon >> day;
-
-        Chrono::Date c_date{year, Chrono::Month(mon), day};
-
-        bool avail = false;
-        cout << "\nEnter the availability of the book (0 if it is absent, 1 otherwise): ";
-
-        cin >> avail;
-
-        Book new_book{author, title, isbn, c_date, avail, gen};
-
-        lib.push_back(new_book);
-
-        cout << "Would you like to add one more book?\n";
-
-        string ans;
-
-        cin >> ans;
-        if (ans != "yes" && ans != "y")
-            break;
     }
 }
